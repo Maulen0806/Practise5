e0bd7a296af231fb431eb4a6b2f698c04cea7119
Maulen0806 <ma_nurzhanuly@kbtu.kz>
2026-03-06 12:25:33 +0600

    Update search.py

--- RegEx/search.py
+++ RegEx/search.py
@@ -1,1 +1,12 @@
-
+import re
+text = "I love python"
+result_1 = re.search(r"\d", text)
+result_2 = re.search(r"\s", text)
+if result_1:
+    print("The string contains digits")
+else:
+    print("the string DOESN'T contain digit")
+if result_2:
+    print("String contains a white space character")
+else:
+    print("String DOESN'T contain a white space character")
